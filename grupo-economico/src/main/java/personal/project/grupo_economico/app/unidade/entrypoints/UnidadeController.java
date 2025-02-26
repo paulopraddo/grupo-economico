ec6e6b04adfaef54f3717a307755ed05880190dd
Paulo Prado <paulopraddo011@gmail.com>
2025-02-26 19:01:42 -0300

    feat updateUnidade

--- grupo-economico/src/main/java/personal/project/grupo_economico/app/unidade/entrypoints/UnidadeController.java
+++ grupo-economico/src/main/java/personal/project/grupo_economico/app/unidade/entrypoints/UnidadeController.java
@@ -6,15 +6,18 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.AllArgsConstructor;
 import personal.project.grupo_economico.app.unidade.restModels.GetUnidadeRestModel;
+import personal.project.grupo_economico.app.unidade.restModels.UpdateUnidadeRestModel;
 import personal.project.grupo_economico.app.unidade.restModels.UploadUnidadeRestModel;
 import personal.project.grupo_economico.domain.unidade.useCases.GetListOfUnidadeUseCase;
 import personal.project.grupo_economico.domain.unidade.useCases.GetUnidadeUseCase;
+import personal.project.grupo_economico.domain.unidade.useCases.UpdateUnidadeUseCase;
 import personal.project.grupo_economico.domain.unidade.useCases.UploadUnidadeUseCase;
 
 @RestController
@@ -25,6 +28,7 @@ public class UnidadeController {
     private final UploadUnidadeUseCase uploadUnidadeUseCase;
     private final GetUnidadeUseCase getUnidadeUseCase;
     private final GetListOfUnidadeUseCase getListOfUnidadeUseCase;
+    private final UpdateUnidadeUseCase updateUnidadeUseCase;
 
     @PostMapping()
     public ResponseEntity<String> uploadUnidade(@RequestBody UploadUnidadeRestModel restModel) {
@@ -42,5 +46,13 @@ public class UnidadeController {
     @GetMapping
     public ResponseEntity<List<GetUnidadeRestModel>> getListOfUnidade() {
         return ResponseEntity.ok().body(this.getListOfUnidadeUseCase.execute());
+    }
+
+    @PutMapping
+    public ResponseEntity<String> updateUnidade(@RequestBody UpdateUnidadeRestModel restModel) {
+
+        this.updateUnidadeUseCase.execute(restModel);
+
+        return ResponseEntity.ok().body("Unidade alterada com sucesso.");
     }
 }
--- grupo-economico/src/main/java/personal/project/grupo_economico/app/unidade/provider/UnidadeProvider.java
+++ grupo-economico/src/main/java/personal/project/grupo_economico/app/unidade/provider/UnidadeProvider.java
@@ -5,8 +5,10 @@ import java.util.List;
 import org.springframework.stereotype.Component;
 
 import lombok.AllArgsConstructor;
+import personal.project.grupo_economico.app.bandeira.provider.entity.BandeiraEntity;
 import personal.project.grupo_economico.app.unidade.provider.entity.UnidadeEntity;
 import personal.project.grupo_economico.app.unidade.repository.UnidadeRepository;
+import personal.project.grupo_economico.app.unidade.restModels.UpdateUnidadeRestModel;
 
 @Component
 @AllArgsConstructor
@@ -27,6 +29,18 @@ public class UnidadeProvider implements UnidadeDataProvider{
     @Override
     public List<UnidadeEntity> getListOfUnidade() {
         return this.repository.findAll();
+    }
+
+    @Override
+    public void updateUnidade(UpdateUnidadeRestModel restModel) {
+        UnidadeEntity entity = this.repository.findById(restModel.getId()).orElse(null);
+
+        entity.setNomeFantasia(restModel.getNomeFantasia());
+        entity.setRazaoSocial(restModel.getRazaoSocial());
+        entity.setCnpj(restModel.getCnpj());
+        entity.setBandeira(BandeiraEntity.builder().id(restModel.getBandeiraId()).build());
+
+        this.repository.save(entity);
     }
     
 
